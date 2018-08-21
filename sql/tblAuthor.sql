ffdb82e4db0a71afa602d13e683d56c739ee08a9
the-graytest <tamblingcourtney@gmail.com>
2018-08-20 20:50:10 -0400

    Added tblAuthor and tblInstruction

---
+++ sql/tblAuthor.sql
@@ -0,0 +1,7 @@
+CREATE TABLE tblAuthor(
+	AuthorId(INT) NOT NULL,
+	AuthorName(VARCHAR(250)) NOT NULL
+);
+
+INSERT INTO tblAuthor(AuthorId, AuthorName)
+VALUES (1, 'Anne')
---
+++ sql/tblInstruction.sql
@@ -0,0 +1,25 @@
+CREATE TABLE tblTimeType (
+	TimeTypeId INT,
+	TimeType VARCHAR(250)
+);
+
+INSERT INTO tblTimeType (TimeTypeId, TimeType) 
+VALUES (1, 'Prep');
+
+INSERT INTO tblTimeType (TimeTypeId, TimeType) 
+VALUES (2, 'Cook');
+
+CREATE TABLE tblInstruction (
+	InstructionText VARCHAR(1000),
+	InstructionStepNumber INT,
+	InstructionId INT,
+	InstructionTime TIME,
+	-- (prep work or cook time), this will be so that I can measure how long a recipe will take
+	InstructionTimeTypeId INT, 
+);
+
+INSERT INTO tblInstruction (InstructionText,InstructionStepNumber,InstructionId,InstructionTime,InstructionTimeType)
+VALUES ("Combine ingredients and form into balls, place onto a lightly greased cookie sheet", 1, 1, 15,1);
+
+INSERT INTO tblInstruction (InstructionText,InstructionStepNumber,InstructionId,InstructionTime,InstructionTimeType)
+VALUES ("Bake for 10-15 minutes at 425 degrees", 2, 2, 15,2);
