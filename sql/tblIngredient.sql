7ca64166d611545fae459ee5a776d6c7a253ebff
the-graytest <tamblingcourtney@gmail.com>
2018-08-20 20:36:49 -0400

    tblRecipe tblIngredient tblplIngredient Creation scripts and Insert statements for first recipe. Because Sausage balls are the greatest contribution of this entire cookbook, ty Jenny

---
+++ sql/tblIngredient.sql
@@ -0,0 +1,31 @@
+CREATE TABLE tblplIngredient(
+	plIngredientId INT,
+	plIngredientName VARCHAR(250),
+	plIngredientNotes VARCHAR(1000)
+);
+
+CREATE TABLE tblIngredient(
+	IngredientId INT,
+	IngredientRecipeId VARCHAR(250),
+	IngredientAmount INT,
+	IngredientUnitSingular VARCHAR(250),
+	IngredientNotes VARCHAR(1000)
+);
+
+INSERT INTO tblplIngredient(plIngredientName, plIngredientId, plIngredientNotes)
+VALUES('Bisquick', 1, null);
+
+INSERT INTO tblplIngredient(plIngredientName, plIngredientId, plIngredientNotes)
+VALUES('Hot Sausage', 2, null);
+
+INSERT INTO tblplIngredient(plIngredientName, plIngredientId, plIngredientNotes)
+VALUES('Cheddar Cheese', 3, null);
+
+INSERT INTO tblIngredient(IngredientId, IngredientRecipeId, IngredientAmount, IngredientUnitSingular, IngredientNotes)
+VALUES (1,1,3,'cup', null);
+
+INSERT INTO tblIngredient(IngredientId, IngredientRecipeId, IngredientAmount, IngredientUnitSingular, IngredientNotes)
+VALUES (1,1,1,'pound',null);
+
+INSERT INTO tblIngredient(IngredientId, IngredientRecipeId, IngredientAmount, IngredientUnitSingular, IngredientNotes)
+VALUES (1,1,8,'ounce','Shredded', null);
